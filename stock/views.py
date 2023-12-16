e9895761d3a96a1c793ceafee0b915e3773f0f61
Pasha Maigutyak <pmaigutyak@gmail.com>
2023-12-16 09:22:20 +0200

    Fix issues.

--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,6 @@
 
 from django.db.models import F
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from cap.decorators import admin_render_view
 from stock.forms import StockReportForm
