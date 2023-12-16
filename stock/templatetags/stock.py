c8284ab236b20f4eb08a8f67d0fca1f692e4b163
Pasha Maigutyak <pmaigutyak@gmail.com>
2023-12-16 09:31:41 +0200

    Update version and improve error handling in stock.py
    The setup version has been updated to 1.0.4. In stock.py, the exception handling has been improved by adding the ValueError catch to the try/except clause. This will enhance the robustness of the code when encountering unexpected situations.

--- stock/templatetags/stock.py
+++ stock/templatetags/stock.py
@@ -11,5 +11,5 @@ def is_category_shown(products, current_index):
             products[int(current_index) - 1].category_id !=
             products[current_index].category_id
         )
-    except (IndexError, AssertionError):
+    except (IndexError, AssertionError, ValueError):
         return True
